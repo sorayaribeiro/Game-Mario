25996f16f6edbf93696edb74ef3a8d151a0eb5a2
Soraya <soraya_alr@yahoo.com.br>
2023-07-01 11:58:01 -0300

    clouds
    Animação de fundo com cor e animação  da cloud sem condição de parada

--- js/script.js
+++ js/script.js
@@ -28,6 +28,8 @@ const loop = setInterval(() => {
             mario.style.width = '75px';
             mario.style.marginLeft = '50px';
 
+            clearInterval(loop);
+
         }
 
     }, 10);
